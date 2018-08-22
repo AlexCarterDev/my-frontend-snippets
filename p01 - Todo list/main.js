0b9cb8257679dfa50563782b8c125e9741be038a
Alex <AlexCarterDeveloper@gmail.com>
2018-08-23 01:59:34 +0400

    Count percentage depends on number of completed tasks

--- p01 - Todo list/main.js
+++ p01 - Todo list/main.js
@@ -2,31 +2,65 @@ var CLS_MATERIAL_ICONS = "material-icons";
 var CLS_DESCRIPTION = "description";
 var CLS_BTN_REMOVE = "btn-remove";
 var CLS_TASK = "task";
+var CLS_UNSELECTABLE = "unselectable";
+var CLS_CHECKBOX = "checkbox";
 
 var taskList = document.querySelector(".todo .task-container");
+var percentage = document.querySelector(".todo .percentage");
 
+function updatePercentage() {
+    var nChilds = taskList.childElementCount;
+    var nCheck = 0;
+    var checkBoxes = taskList.getElementsByClassName(CLS_CHECKBOX);
+    
+    for (let i = 0; i < checkBoxes.length; i++) {
+        var cb = checkBoxes[i];
+        nCheck += cb.state;
+    }
+    
+    if (nChilds == 0) {
+        percentage.innerHTML = "";
+    } else {
+        percentage.innerHTML = Math.round((nCheck/nChilds)*100) + "%";
+    }
+}
+
+function updateCheckBoxUI(checkBox) {
+    if (checkBox.state == 1) {
+        checkBox.innerHTML = "check_box";
+    } else {
+        checkBox.innerHTML = "check_box_outline_blank";
+    }
+}
 
 function createNewTask(text) {
     var task = document.createElement("li");
     task.className = CLS_TASK;
 
     var dragBtn = document.createElement("i");
-    dragBtn.className = CLS_MATERIAL_ICONS;
+    dragBtn.className = CLS_MATERIAL_ICONS + " " + CLS_UNSELECTABLE;
     dragBtn.innerHTML = "drag_indicator";
 
     var checkBox = document.createElement("i");
-    checkBox.className = CLS_MATERIAL_ICONS;
-    checkBox.innerHTML = "check_box_outline_blank";
+    checkBox.state = 0; // add new feature: checkbox checked or not
+    updateCheckBoxUI(checkBox);
+    checkBox.className = CLS_MATERIAL_ICONS + " " + CLS_UNSELECTABLE + " " + CLS_CHECKBOX;
+    checkBox.onclick = function() {
+        checkBox.state = checkBox.state ? 0 : 1;
+        updateCheckBoxUI(checkBox);
+        updatePercentage();
+    };
 
     var descr = document.createElement("span");
     descr.className = CLS_DESCRIPTION;
     descr.innerHTML = text;
 
     var removeBtn = document.createElement("i");
-    removeBtn.className = CLS_MATERIAL_ICONS + " " + CLS_BTN_REMOVE;
+    removeBtn.className = CLS_MATERIAL_ICONS + " " + CLS_BTN_REMOVE + " " + CLS_UNSELECTABLE;
     removeBtn.innerHTML = "remove_circle";
     removeBtn.onclick = function() {
         task.parentElement.removeChild(task);
+        updatePercentage();
     };
 
     task.appendChild(dragBtn);
@@ -37,9 +71,8 @@ function createNewTask(text) {
     taskList.appendChild(task);
 }
 
-
 createNewTask("Feed my cat");
 createNewTask("Learn english");
 createNewTask("Clean room");
 
-
+updatePercentage();
