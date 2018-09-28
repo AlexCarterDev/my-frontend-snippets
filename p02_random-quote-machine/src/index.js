256809e184d7d211127e2e7a09960b389f685c8c
Alex <AlexCarterDeveloper@gmail.com>
2018-09-28 20:05:25 +0400

    A Static Version in React

--- p02_random-quote-machine/src/index.js
+++ p02_random-quote-machine/src/index.js
@@ -3,18 +3,68 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.scss';
 
-
-class App extends React.Component {
-    
-
+class Text extends React.Component {
     render() {
         return (
-            <div>
-                Hello
+            <p id='text'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Nesciunt quam aliquid nihil eos ea ex porro cupiditate
+                officia
+            </p>
+        );
+    }
+}
+
+class Separator extends React.Component {
+    render() {
+        return (
+            <div id='separator-container'>
+                <div id='separator'></div>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Author extends React.Component {
+    render() {
+        return (
+            <p id='author'>
+                Lorem lorem
+            </p>
+        );
+    }
+}
+
+class Buttons extends React.Component {
+    render() {
+        return(
+            <div id='buttons'>
+                <button id="tweet-quote">
+                    <i className="fab fa-tumblr"></i>
+                </button>
+                <button id="tumblr-quote">
+                    <i className="fab fa-twitter"></i>
+                </button>
+                <button id="new-quote">
+                    New quote
+                </button>
+            </div>
+        );
+    }
+}
+
+class QuoteBox extends React.Component {
+    render() {
+        return (
+            <div id='quote-box'>
+                <Text/>
+                <Separator/>
+                <Author/>
+                <Buttons/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<QuoteBox />, document.getElementById('root'));
 registerServiceWorker();
