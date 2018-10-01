f192f355f12fc714de21f815d0c57b464ec7542d
Alex <AlexCarterDeveloper@gmail.com>
2018-10-02 01:15:34 +0400

    Remove react demo files

--- p03_markdown-previewer/src/index.js
+++ p03_markdown-previewer/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
