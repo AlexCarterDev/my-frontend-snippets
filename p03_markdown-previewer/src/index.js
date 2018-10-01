ecae07f8f9cffb98cb3e050483d803b0e2b30c22
Alex <AlexCarterDeveloper@gmail.com>
2018-10-02 01:34:58 +0400

    Include bootstrap library

--- p03_markdown-previewer/src/index.js
+++ p03_markdown-previewer/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class App extends React.Component {
     render() {
         return (
             <div>
-                Hello
+                <button className="btn btn-primary">
+                    Primary bootstrap button
+                </button>
             </div>
         );
     }
