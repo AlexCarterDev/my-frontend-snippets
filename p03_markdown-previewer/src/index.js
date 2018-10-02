31e35aef5650289b3a8b82167c128314f7f70146
Alex <AlexCarterDeveloper@gmail.com>
2018-10-03 02:05:17 +0400

    Style components

--- p03_markdown-previewer/src/index.js
+++ p03_markdown-previewer/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
 class Preview extends React.Component {
     render() {
         return(
-            <textarea id='preview' style={{width: 400, height: 200}}>
+            <textarea id='preview' readonly='true'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis laborum, vel similique quidem repudiandae autem iure? Ipsum, dolorum harum soluta sit illum voluptatum delectus exercitationem impedit odio, quasi a error accusantium dicta, voluptatibus ad aliquid voluptate deserunt recusandae dolores saepe!
             </textarea>
         )
@@ -24,7 +25,7 @@ class Title extends React.Component {
 class Editor extends React.Component {
     render() {
         return (
-            <textarea id='editor' style={{width: 400, height: 200}}>
+            <textarea id='editor'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium minus odio ratione pariatur illum sed tempora nulla dolor eius praesentium dolorem, provident sint minima qui sequi necessitatibus distinctio quo voluptas inventore? Distinctio et odit consectetur voluptate sequi architecto iure.
             </textarea>
         )
