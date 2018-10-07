8e2bc401b955657a4571ae63bf6cef4dd4982d96
Alex <AlexCarterDeveloper@gmail.com>
2018-10-07 23:18:28 +0400

    Install sass

--- p04_drum-machine/src/index.js
+++ p04_drum-machine/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.scss';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
