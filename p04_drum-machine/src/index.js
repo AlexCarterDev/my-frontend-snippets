cf7a9d33a94759029b98aa085874ffea471376e2
Alex <AlexCarterDeveloper@gmail.com>
2018-10-08 04:35:01 +0400

    Create component: AudioButton

--- p04_drum-machine/src/index.js
+++ p04_drum-machine/src/index.js
@@ -2,11 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
-class App extends React.Component {
+class AudioButton extends React.Component {
+    componentDidMount() {
+        console.log('AudioButton#' + this.props.buttonId + ': didMount');
+        document.addEventListener('keydown', (event) => {
+            if (event.keyCode === this.props.keyCode) {
+                this.props.clickHandler(event, this.props.audioDescr);
+            }
+        })
+    }
+
     render() {
+        console.log('AudioButton#' + this.props.buttonId + ': render');
+        const onClickHandler = (event) => this.props.clickHandler(event, this.props.audioDescr);
+
+        return (
+            <button id={this.props.buttonId} onClick={onClickHandler}>
+                {this.props.text}
+                <audio>
+                    <source src={this.props.audioSrc} />
+                </audio>
+            </button>
+        )
+    }
+}
+
+class App extends React.Component {
+
+    log(event, text) {
+        console.log('button clicked ' + text);
+        console.log(event);
+    }
+
+    componentDidMount() {
+        console.log('App: didMount');
+    }
+
+    render() {
+        console.log('App: render');
         return(
             <div>
-
+                <AudioButton 
+                    buttonId='my-button' 
+                    keyCode={87} 
+                    text='test button'
+                    audioSrc='#'
+                    clickHandler={this.log} 
+                    audioDescr='empty'/>
             </div>
         )
     }
