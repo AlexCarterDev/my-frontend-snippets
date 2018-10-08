5121d3bfc0d53efe11e64fe91739079802e69099
Alex <AlexCarterDeveloper@gmail.com>
2018-10-09 01:16:40 +0400

    Describe react components structure

--- p04_drum-machine/src/index.js
+++ p04_drum-machine/src/index.js
@@ -2,27 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
+
+
+// --- React Components ---
+
 class AudioButton extends React.Component {
-    componentDidMount() {
-        console.log('AudioButton#' + this.props.buttonId + ': didMount');
-        document.addEventListener('keydown', (event) => {
-            if (event.keyCode === this.props.keyCode) {
-                this.props.clickHandler(event, this.props.audioDescr);
-            }
-        })
+    render() {
+        console.log('AudioButton ' + this.props.id + ': render');
+
+        return (
+            <button id={this.props.id}>
+                X
+                <audio>
+                    <source src='#' />
+                </audio>
+            </button>
+        )
+    }
+}
+
+
+class DrumPad extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.buttons = this.createAudioButtons(9);
+    }
+
+    createAudioButtons(n) {
+        let buttons = [];
+        for (let i = 0; i < n; i++) {
+            buttons.push(
+                <AudioButton 
+                    key={i}
+                    id={'drum-pad-' + i}/>
+            );
+        }
+
+        return buttons;
     }
 
     render() {
-        console.log('AudioButton#' + this.props.buttonId + ': render');
-        const onClickHandler = (event) => this.props.clickHandler(event, this.props.audioDescr);
-
-        return (
-            <button id={this.props.buttonId} onClick={onClickHandler}>
-                {this.props.text}
-                <audio>
-                    <source src={this.props.audioSrc} />
-                </audio>
-            </button>
+        return(
+            <div id='drum-pad'>
+                {this.buttons}
+            </div>
         )
     }
 }
@@ -37,7 +61,73 @@ class Title extends React.Component {
     }
 }
 
+class Display extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>display</span>
+      </div>
+    )
+  }
+}
 
+class SelectBankButton extends React.Component {
+    render() {
+        return(
+            <button id={this.props.id}>bank</button>
+        )
+    }
+}
+
+class BankPad extends React.Component {
+    constructor(props) {
+        super(props);
+        this.buttons = this.createSelectButtons(3);
+    }
+
+    createSelectButtons(n) {
+        let buttons = [];
+        for (let i = 0; i < n; i++) {
+            buttons.push(
+                <SelectBankButton 
+                    key={i}
+                    id={'bank-pad-' + i}/>
+            );
+        }
+
+        return buttons;
+    }
+
+    render() {
+      return (
+        <div>
+            {this.buttons}
+        </div>
+      )
+    }
+  }
+
+  class Volume extends React.Component {
+    render() {
+      return (
+        <div>
+          volume
+        </div>
+      )
+    }
+  }
+
+class Panel extends React.Component {
+    render() {
+        return (
+            <div id='panel'>
+                <Display />
+                <BankPad />
+                <Volume />
+            </div>
+        );
+    }
+}
 
 class App extends React.Component {
 
@@ -51,7 +141,8 @@ class App extends React.Component {
         return(
             <div>
                 <Title title='Drum machine' />
-
+                <Panel />
+                <DrumPad />
             </div>
         )
     }
