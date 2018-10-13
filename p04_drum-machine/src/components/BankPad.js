3f88863ddc06793008e7676ed935fd14b8c404eb
Alex <AlexCarterDeveloper@gmail.com>
2018-10-13 19:27:51 +0400

    BankPad buttons behave like radio group

--- p04_drum-machine/src/components/BankPad.js
+++ p04_drum-machine/src/components/BankPad.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import SelectBankButton from './SelectBankButton';
 
-const BankPad = () => {
-    const buttons = [];
-    const texts = ['bank 1', 'bank 2', 'bank 3'];
-    for (let i = 0; i < texts.length; i++) {
-        buttons.push(
-            <SelectBankButton 
-                text={texts[i]}
-                key={i}
-                id={'bank-pad-' + i}/>
-        );
+
+export default class BankPad extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedBank: 0
+        }
+        this.clickHandler = this.clickHandler.bind(this);
     }
 
-    return (
-        <div id='bank-pad'>
-            {buttons}
-        </div>
-    )
-}
+    clickHandler(id) {
+        var bank = 0;
+        var n = parseInt(id.substring(id.length - 1), 10);
+        this.setState({
+            selectedBank: n
+        })
+    }
 
-export default BankPad;
+    render() {
+        const buttons = [];
+        const texts = ['bank 1', 'bank 2', 'bank 3'];
+
+        for (let i = 0; i < texts.length; i++) {
+            buttons.push(
+                <SelectBankButton 
+                    text={texts[i]}
+                    key={i}
+                    id={'bank-pad-' + i}
+                    onClick={this.clickHandler}
+                    checked={i === this.state.selectedBank} />
+            );
+        }
+
+        return (
+            <div>
+                {buttons}
+            </div>
+        )
+    }
+}
--- p04_drum-machine/src/components/SelectBankButton.js
+++ p04_drum-machine/src/components/SelectBankButton.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const SelectBankButton = ({id, text}) => {
+const SelectBankButton = ({id, text, onClick, checked}) => {
+    var className = checked ? 
+        'button-blue button-blue-active' : 
+        'bank-pad-button-not-selected';
+
     return(
         <button 
-            className='button-blue'
+            className={className}
             id={id}
+            onClick={() => onClick(id)}
         >
             {text}
         </button>
