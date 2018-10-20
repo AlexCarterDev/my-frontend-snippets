7868b5b180c6aae59b1a15e6797bc254404146d1
Alex <AlexCarterDeveloper@gmail.com>
2018-10-20 15:44:54 +0400

    Remove demo files

--- p05_javascript-calculator/src/index.js
+++ p05_javascript-calculator/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
